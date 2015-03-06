6a242b9d3f9cc53f570bc8f632d4d6ea000ee728
krazevedo <kaio.azevedo85@gmail.com>
2015-03-06 09:45:16 -0300

    nova classe de datadriven

---
+++ src/main/java/com/ciandt/selenium/redenatura/helpers/DataDriven.java
@@ -0,0 +1,26 @@
+package com.ciandt.selenium.redenatura.helpers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DataDriven {
+	 public Properties obj;
+	    
+		public void lerArquivo() throws IOException {
+
+			//Create Object of Properties Class.
+			obj = new Properties();   
+			//Create Object of FileInputStream Class. Pass file path.
+			File file = new File("src\\main\\resources\\regressao.properties");
+			FileInputStream fileInput = null;
+			fileInput = new FileInputStream(file);
+			//Pass object reference objfile to load method of Properties object.
+			obj.load(fileInput); 
+		}
+		
+		public Properties getProperties(){
+			return obj;
+		}
+}
